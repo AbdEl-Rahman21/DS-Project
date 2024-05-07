b43a007869dc8cecbd206cbddcb44d88e8387752
AbdEl-Rahman21 <abdel292004@gmail.com>
2024-05-07 18:54:20 +0300

    Add LevelMap Class

---
+++ LevelMap.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <list>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <Windows.h>
+#include <unordered_map>
+
+using namespace std;
+
+class MapNode {
+private:
+	bool status;
+	list<int> children;
+public:
+	MapNode(list<int> children);
+	bool getStatus();
+	void setStatus(bool newStatus);
+	list<int> getChildren();
+	~MapNode();
+};
+
+class LevelMap {
+private:
+	int rowLength;
+	int columnLength;
+
+	int eatenPoints;
+	int remainingPoints;
+
+	unordered_map<int, MapNode> map;
+public:
+	LevelMap(vector<int> mapData);
+	void createMap(vector<int> mapData);
+	list<int> findChildren(int currentNode, vector<int> mapData);
+	bool isWall(int child, vector<int> mapData);
+	void printMap(int playerPosition, int ghostPositions[]);
+	bool printGhost(int currentNode, int ghostPositions[]);
+;	void updateMap(int playerPosition);
+	~LevelMap();
+};
