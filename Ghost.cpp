fb15289d68417e9e0f8f87e559e08c88df28c4d0
AbdEl-Rahman21 <abdel292004@gmail.com>
2024-05-08 18:11:42 +0300

    Move ghost functions to ghost class

--- Ghost.cpp
+++ Ghost.cpp
@@ -1,1 +1,66 @@
 #include "Ghost.h"
+
+GhostNode::GhostNode() {
+	position = 0;
+	parent = nullptr;
+}
+
+GhostNode::GhostNode(int position, GhostNode* parent) {
+	this->position = position;
+	this->parent = parent;
+}
+
+int GhostNode::traceParent() {
+    GhostNode* currentNode = this;
+
+    while (currentNode->parent->parent != nullptr) {
+        currentNode = currentNode->parent;
+    }
+
+    return currentNode->position;
+}
+
+GhostNode::~GhostNode() {
+	parent = nullptr;
+}
+
+Ghost::Ghost() {
+	position = 0;
+}
+
+Ghost::Ghost(int position) {
+	this->position = position;
+}
+
+// Hard Mode Using BFS Algorithm
+int Ghost::getHardMove(int playerPosition, unordered_map<int, MapNode>& map) {
+    queue<GhostNode> nodes;
+    GhostNode* currentNode = nullptr;
+
+    nodes.push(GhostNode(position, nullptr));
+
+    currentNode = &nodes.front();
+
+    while (!nodes.empty()) {
+        // Children of each node insertion in the queue
+        for (auto child : map[currentNode->position].getChildren()) {
+            nodes.push(GhostNode(child, currentNode));
+        }
+
+        nodes.pop();
+
+        currentNode = &nodes.front();   // Update currentNode
+
+        // Pac-Man Postionition is found
+        if (playerPosition == currentNode->position) {
+            return currentNode->traceParent();  // Return first move in shortest path
+        }
+    }
+
+    // Pac-Man position is not found
+    return -1;
+}
+
+Ghost::~Ghost() {
+	position = 0;
+}
--- Ghost.h
+++ Ghost.h
@@ -1,5 +1,32 @@
 #pragma once
-class Ghost
-{
+
+#include "LevelMap.h"
+
+#include <list>
+#include <queue>
+#include <unordered_map>
+
+using namespace std;
+
+class GhostNode {
+public:
+	int position;
+	GhostNode* parent;
+public:
+	GhostNode();
+	GhostNode(int position, GhostNode* parent);
+	int traceParent();
+	~GhostNode();
 };
 
+class Ghost {
+private:
+	int position;
+public:
+	Ghost();
+	Ghost(int position);
+	int getEasyMove();
+	int getMediumMove();
+	int getHardMove(int playerPosition, unordered_map<int, MapNode>& map);
+	~Ghost();
+};
